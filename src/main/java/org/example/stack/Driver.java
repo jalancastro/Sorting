c64bbe5a2b9291f0d1502fc2a1ad514eb6468f25
jalancastro <alan.castro44@gmail.com>
2023-03-09 18:22:00 -0700

    stack code added

---
+++ src/main/java/org/example/stack/Driver.java
@@ -0,0 +1,28 @@
+package org.example.stack;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new ArrayListStack<>();
+
+        stack.push("One");
+        stack.push("Two");
+        stack.push("Three");
+
+        System.out.println(stack);
+        System.out.println("");
+        System.out.println("Empty?: " + stack.empty());
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println("Empty?: " + stack.empty());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.empty());
+        System.out.println(stack.pop());
+
+
+
+    }
+}
